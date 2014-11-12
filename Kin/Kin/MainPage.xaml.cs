df52000882400208b4d96b1fa2c1e5db1e25e698
Jorge Lopez <jorge.lopez.18820@gmail.com>
2014-11-12 17:59:02 +0900

    First Commit
    Firt Commit

---
+++ Kin/Kin/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Kin.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Kin
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            InitializeComponent();
+            //This is where the magic happens
+            //Fill the viewModel with the data
+            var viewModel = new MainPageViewModel { CurrentConditions  = MainPageViewModel.AsyncFetchData() };
+            //Assign the viewModel with the data to the DataContext
+            //The bindings will be automatically done in the XAML
+            DataContext = viewModel;
+        }
+    }
+}
---
+++ Kin/Kin/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Kin.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kin.ViewModel
+{
+    class MainPageViewModel
+    {
+        public List<WeatherCurrent> CurrentConditions { get; set; }
+
+        public MainPageViewModel()
+        {
+            CurrentConditions = new List<WeatherCurrent>();
+        }
+
+
+        public static List<WeatherCurrent> FetchData()
+        {
+            List<WeatherCurrent> lst = AsyncFetchData().Result;
+
+            return lst;
+
+            //var task = Task.Run(async () =>
+            //{
+            //    return await AsyncFetchData();
+            //});
+            //return task.Result;
+        }
+
+        public async static Task<List<WeatherCurrent>> AsyncFetchData()
+        //private static async void AsyncFetchData()
+        {
+
+            HttpClient client = new HttpClient();
+            client.MaxResponseContentBufferSize = 256000;
+            HttpResponseMessage response = await client.GetAsync("http://api.openweathermap.org/data/2.5/weather?q=Seoul,kr&units=metric");
+            response.EnsureSuccessStatusCode();
+            
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+            WeatherCurrent weather = JsonConvert.DeserializeObject<WeatherCurrent>(jsonResponse);
+
+            List<WeatherCurrent> lst = new List<WeatherCurrent>();
+
+            lst.Add(weather);
+
+            return lst;
+        }
+    }
+}
